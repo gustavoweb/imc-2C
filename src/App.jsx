2bd923daabdaf3c2334f30d3a3c395e7d499f44b
gustavoweb <contato@gustavoweb.com>
2026-02-11 09:03:34 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+     <div className="imc-container">
+      <div className="imc-card">
+        <div className="imc-header">
+          <h1>Calculadora IMC</h1>
+          <p>Descubra seu índice de massa corporal</p>
+        </div>
+
+        <div className="imc-form">
+          <input 
+            type="number" 
+            placeholder='Ex: 70' 
+          />
+          <input type="number" 
+            placeholder='Ex: 1.75' 
+          />
+          <button>Calcular</button>
+          <button>Limpar</button>
+        </div>
+
+      </div> {/* Fecha Card */}
+     </div> {/* Fecha Container */}
+    </>
+  )
+}
+
