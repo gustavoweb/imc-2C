3535a387cdb515d117acbca899582e8535691f78
gustavoweb <contato@gustavoweb.com>
2026-02-19 08:23:39 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,33 @@ import './App.css'
 export default function App() {
   return (
     <>
-      <div className="imc-container">
-        <div className="imc-card">
-          <div className="imc-header">
-            <h1></h1>
-            <h1>caucolando peso corporal</h1>   </div>
-          <div className='imc-form'></div>
+     <div className="imc-container">
+      <div className="imc-card">
+        <div className="imc-header">
+          <h1>Calculadora IMC</h1>
+          <p>Descubra seu índice de massa corporal</p>
+        </div>
+
+        <div className="imc-form">
           <input 
-          type="number" 
-          placeholder='ex:70'/>
-          <input 
-          type="number" 
-          placeholder='ex:1.75'/>
-          <button>Caucular</button>
-          <button>limpar</button>
-        </div> 
-      </div> 
+            type="number" 
+            placeholder='Ex: 70' 
+          />
+          <input type="number" 
+            placeholder='Ex: 1.75' 
+          />
+          <button>Calcular</button>
+          <button>Limpar</button>
+        </div>
+
+        <div className="result-card">
+         <h1>22.9</h1>
+         <p>Peso Normal</p>
+         <small>Índice de Massa Corporal</small>
+        </div>
+
+      </div> {/* Fecha Card */}
+     </div> {/* Fecha Container */}
     </>
   )
 }
